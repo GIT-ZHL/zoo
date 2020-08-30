01e6724544a8d25d63d188614b97a51c17db185a
zhaohaoliang <739399758@qq.com>
2020-08-30 14:09:26 +0800

    init project

--- polar-bear/polar-bear-server/src/test/java/org/haoliang/zoo/bear/controller/BearControllerTest.java
+++ polar-bear/polar-bear-server/src/test/java/org/haoliang/zoo/bear/controller/BearControllerTest.java
@@ -4,7 +4,5 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:applicationContext.xml"})
 public class BearControllerTest {
 }
--- polar-bear/polar-bear-server/src/test/java/org/haoliang/zoo/bear/service/BearServiceTest.java
+++ polar-bear/polar-bear-server/src/test/java/org/haoliang/zoo/bear/service/BearServiceTest.java
@@ -4,7 +4,5 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration({"classpath:applicationContext.xml"})
 public class BearServiceTest {
 }
